fa8f1c590bbe9bda23671f6d3d1c692f0468ac55
gedesurya125 <gedesurya125@gmail.com>
2024-08-18 23:50:04 +0800

    add some information

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,9 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+// import {presentationTool} from 'sanity/presentation'
 
+const SANITY_STUDIO_PREVIEW_URL = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
 export default defineConfig({
   name: 'default',
   title: 'portofolio',
@@ -10,7 +12,15 @@ export default defineConfig({
   projectId: 'hu9rbuh3',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //? it used to add the preview page
+    // presentationTool({
+    //   // Required: set the base URL to the preview location in the front end
+    //   previewUrl: SANITY_STUDIO_PREVIEW_URL,
+    // }),
+  ],
 
   schema: {
     types: schemaTypes,
